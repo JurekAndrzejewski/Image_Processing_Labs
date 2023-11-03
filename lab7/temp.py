edb7103f4a8b29f7da9bb16ac5ff13b95dcb58f5
JerzyAndrzejewski <jerzy.andrzejewski00@gmail.com>
2023-11-03 14:37:22 +0100

    first commit

---
+++ lab7/temp.py
@@ -0,0 +1,32 @@
+a = 1
+print(f'Place in memory before addition {id(a)}')
+a += 1
+print(f'Place in memory after addition {(id(a))}')
+
+'''
+def valid(aqq):
+    print('')
+def transaction(aqq):
+    print('')
+card_number = order = error_message = 1
+
+if valid(card_number):
+    transaction(card_number, order)
+else:
+    print(error_message)
+
+
+
+list_names = ['Jason', 'Jackson', 'Albert', 'Michael', 'Jessica'] # First block of code (list initialization)
+
+J_counter = 0 # Second and third blocks of code (variables initialization)
+Ja_counter = 0
+
+for name in list_names: # Fourth block of code starts here (for loop)
+ if name[0] == 'J': # Fifth block of code starts here (if statement)
+          J_counter += 1
+          if name[1] == 'a': # Sixth block of code starts here (another if statement)
+                                Ja_counter += 1
+
+print(f'Number of names that starts with \"J\" {J_counter}') # Seventh and Eighth block of code starts here (usage of print functions)
+print(f'Number of names that starts with \"Ja\" {Ja_counter}')'''
